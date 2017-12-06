2ad8de0b3ae6d74157a8769d89a91c408463e246
Carlos Alberto <carmantillab@gmail.com>
2017-12-06 01:09:03 -0500

    Editar datos listo.

--- src/main/java/com/cmancode/app/HomeController.java
+++ src/main/java/com/cmancode/app/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +29,24 @@ public class HomeController {
 	public ModelAndView viewClient(ModelAndView model) {
 		Cliente cliente = new Cliente();
 		model.addObject("cliente", cliente);
+		model.addObject("encabezado", "Nuevo Cliente");
+		model.addObject("boton", "Registrar Cliente");
+		model.setViewName("cliente");
+		return model;
+	}
+	
+	@RequestMapping(value = "/cliente/{id}", method = RequestMethod.GET)
+	public ModelAndView updateClient(@PathVariable("id") Long id, ModelAndView model) {
+		Cliente cliente = null;
+		if(id >0) {
+			cliente = clienteService.findCliente(id);
+		}else {
+			model.setViewName("lista");
+			return model;
+		}
+		model.addObject("cliente", cliente);
+		model.addObject("encabezado", "Modificar datos");
+		model.addObject("boton", "Actualzar datos");
 		model.setViewName("cliente");
 		return model;
 	}
